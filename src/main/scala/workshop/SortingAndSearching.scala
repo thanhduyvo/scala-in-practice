53165d7685fd9cdc03131cdfb7d75a7c56bb743f
thanhduyvo <thanhduy2b@gmail.com>
2019-06-26 13:40:27 -0500

    Update source code

--- src/main/scala/workshop/SortingAndSearching.scala
+++ src/main/scala/workshop/SortingAndSearching.scala
@@ -1,20 +1,27 @@
 package workshop
 
+import workshop.SortingAndSearching._
 import scala.math.Ordering
 
 object SortingAndSearchingRunner {
 
   def main(args: Array[String]): Unit = {
 
+    val fruits = List("apple", "pineapple", "orange", "banana")
+    val numbers = List(8, 6, 4, 9, 1, 2)
+
     // Test
-    println(SortingAndSearching.msort(List(8, 6, 4, 9, 1, 2)))
+    println(msort(numbers))
+    println(msort(fruits))
+
+    println(qsort(numbers))
   }
 
 }
 
 object SortingAndSearching {
 
-  // Merge Sort
+  // Problem 1: Implement Merge Sort algorithm
   def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if(n == 0) xs
@@ -31,7 +38,16 @@ object SortingAndSearching {
     }
   }
 
-  val fruits = List("apple", "pineapple", "orange", "banana")
-  val sortedList = msort(fruits)
-  println(sortedList)
+  // Problem 2: Implement Quick Sort algorithm
+  def qsort(xs: List[Int]): List[Int] = {
+    if(xs.length <= 1) xs
+    else {
+      val pivot = xs(xs.length / 2)
+      List.concat(
+        qsort(xs filter (pivot >)),
+        xs filter (pivot ==),
+        qsort(xs filter (pivot <))
+      )
+    }
+  }
 }
