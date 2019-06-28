91a93521458bc0f1a032ec5249d28b329f20bf86
Dustin Vo <thanhduy2b@gmail.com>
2019-06-28 01:34:03 -0500

    Update source code

--- src/main/scala/workshop/SortingAndSearching.scala
+++ src/main/scala/workshop/SortingAndSearching.scala
@@ -17,10 +17,11 @@ object SortingAndSearchingRunner {
     println(qsort(numbers))
 
     val logLines1 = List("[mi2 jog mid pet]", "[wz3 34 54 398]", "[a1 alps cow bar]", "[x4 45 21 7]")
-    val logLines2 = List("[t2 13 121 98]", "[r1 box ape bit]", "[b4 xi me nu]", "[br8 eat nim did]", "[w1 has uni gry]", "[f3 52 54 31]")
-    println(sortLogLines(logLines1))
-  }
+    println(sortLogLines(4, logLines1))
 
+    val logLines2 = List("[t2 13 121 98]", "[r1 box ape bit]", "[b4 xi me nu]", "[br8 eat nim did]", "[w1 has uni gry]", "[f3 52 54 31]")
+    println(sortLogLines(6, logLines2))
+  }
 }
 
 object SortingAndSearching {
@@ -55,30 +56,42 @@ object SortingAndSearching {
     }
   }
 
+  def sortLogLines(logFileSize: Int, logLines: List[String]): List[String] = {
+    if(logLines.length == logFileSize) {
+      qsortLogLines(logLines).map(line => line.substring(1, line.length - 1))
+    }
+    else logLines
+  }
+
   // Problem 3: Write an algorithm to reorder the data in the log file,, according to defined rules
-  def sortLogLines(logLines: List[String]): List[String] = {
+  def qsortLogLines(logLines: List[String]): List[String] = {
+
     if(logLines.length <= 1) logLines
     else {
       def isAllDigits(x: String) = x.matches("^\\d+$")
 
       def compareLogLine(s1: String, s2: String): Int = {
-        val split1 = s1.split(' ')(1)
-        val split2 = s2.split(' ')(1)
-        if(!isAllDigits(split1) && isAllDigits(split2)) 1
-        else if(isAllDigits(split1) && !isAllDigits(split2)) -1
+        val arr1 = s1.split(' ').tail // get s1 as array without identifier
+        val arr2 = s2.split(' ').tail // get s2 as array without identifier
+
+        // case when head 1 is string and head 2 is number
+        if(!isAllDigits(arr1.head) && isAllDigits(arr2.head)) -1
+
+        // case when head 1 is number and head 2 is string
+        else if(isAllDigits(arr1.head) && !isAllDigits(arr2.head)) 1
+
+        // case when both head 1 and head 2 is string or number
         else {
-          val comp1 = s1.split(' ').tail.toString
-          val comp2 = s2.split(' ').tail.toString
-          if(comp1.compareTo(comp2) != 0) comp1.compareTo(comp2)
+          if(arr1.mkString.compareTo(arr2.mkString) != 0) arr1.mkString.compareTo(arr2.mkString)
           else s1.compareTo(s2)
         }
       }
 
       val pivot = logLines(logLines.length / 2)
       List.concat(
-        sortLogLines(logLines filter(line => compareLogLine(line, pivot) < 0)),
+        qsortLogLines(logLines filter(line => compareLogLine(line, pivot) < 0)),
         logLines filter(line => compareLogLine(line, pivot) == 0),
-        sortLogLines(logLines filter(line => compareLogLine(line, pivot) > 0))
+        qsortLogLines(logLines filter(line => compareLogLine(line, pivot) > 0))
       )
     }
   }
